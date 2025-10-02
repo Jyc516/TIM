38a0f9b692338035976cac5dcd5539d55cff360e
Jyc516 <jxxx17jyc@163.com>
2025-10-02 10:23:15 +0800

    feat: PWM & IWDG - 使用PWM控制LEDR - 添加按键看门狗

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,10 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+#include <math.h>
+
+#include "iwdg.h"
 #include "tim.h"
 #include "gpio.h"
 
@@ -88,9 +92,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_IWDG_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim1);
-
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -100,23 +104,31 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    int counter = __HAL_TIM_GET_COUNTER(&htim1);
-    int max_cnt = __HAL_TIM_GET_AUTORELOAD(&htim1);
-    if (counter < max_cnt / 4) {
-      HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_RESET);
-    }
-    else if (counter < max_cnt / 2) {
-      HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_RESET);
-    }
-    else if (counter < max_cnt * 3 / 4) {
-      HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_SET);
-    }
-    else {
-      HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_SET);
+    // int counter = __HAL_TIM_GET_COUNTER(&htim1);
+    // int max_cnt = __HAL_TIM_GET_AUTORELOAD(&htim1);
+    // if (counter < max_cnt / 4) {
+    //   HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_RESET);
+    //   HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_RESET);
+    // }
+    // else if (counter < max_cnt / 2) {
+    //   HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_SET);
+    //   HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_RESET);
+    // }
+    // else if (counter < max_cnt * 3 / 4) {
+    //   HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_SET);
+    //   HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_SET);
+    // }
+    // else {
+    //   HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_RESET);
+    //   HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin, GPIO_PIN_SET);
+    // }
+
+    uint32_t arr_value = __HAL_TIM_GetAutoreload(&htim1) + 1;
+    uint32_t brightness = arr_value * sinf(4 * HAL_GetTick() / 1000.f) - 1;
+    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, brightness);
+
+    if (HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin) == GPIO_PIN_SET) {
+      HAL_IWDG_Refresh(&hiwdg);
     }
   }
   /* USER CODE END 3 */
@@ -139,8 +151,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 6;
