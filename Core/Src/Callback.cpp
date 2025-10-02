eaff4f0cfc6b5128804198ecacba8a57e1b40fa1
Jyc516 <jxxx17jyc@163.com>
2025-10-02 14:12:50 +0800

    fix: EXTI2按键中断的防抖 - 写起来才发现和昨天的按键防抖逻辑不太一样。只能勉强根据时间间隔写一下。

--- Core/Src/Callback.cpp
+++ Core/Src/Callback.cpp
@@ -7,11 +7,18 @@
 
 #include "cmath"
 
+uint32_t pre_ticks = HAL_GetTick();
+uint32_t cur_ticks;
 volatile uint32_t brightness;
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-    uint32_t arr_value = __HAL_TIM_GET_AUTORELOAD(&htim1) + 1;
-    brightness = arr_value * sinf(4 * HAL_GetTick() / 1000.f) - 1;
-    // __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, brightness);
+    cur_ticks = HAL_GetTick();
+    if (cur_ticks - pre_ticks > 500) {
+        pre_ticks = cur_ticks;
+
+        uint32_t arr_value = __HAL_TIM_GET_AUTORELOAD(&htim1) + 1;
+        brightness = arr_value * sinf(4 * HAL_GetTick() / 1000.f) - 1;
+        // __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, brightness);
+    }
 }
 
 uint32_t count = 0;
